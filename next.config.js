c5759dcf19ebc177efefdd65c70226ae80c074cf
Joel <joel.patane@rib-software.com>
2024-01-08 12:06:05 +1000

    static test

--- next.config.js
+++ next.config.js
@@ -2,34 +2,34 @@
 //const nextConfig = {output: "standalone"}
 
 //module.exports = nextConfig
-// const cspHeader = `
-//     default-src 'self';
-//     script-src 'self' 'unsafe-eval' 'unsafe-inline';
-//     style-src 'self' 'unsafe-inline';
-//     img-src 'self' blob: data:;
-//     font-src 'self';
-//     object-src 'none';
-//     base-uri 'self';
-//     form-action 'self';
-//     frame-ancestors 'none';
-//     block-all-mixed-content;
-//     upgrade-insecure-requests;
-// `
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    block-all-mixed-content;
+    upgrade-insecure-requests;
+`
  
 module.exports = {
-//   async headers() {
-//     return [
-//       {
-//         source: '/(.*)',
-//         headers: [
-//           {
-//             key: 'Content-Security-Policy',
-//             value: cspHeader.replace(/\n/g, ''),
-//           },
-//         ],
-//       },
-//     ]
-//   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+        ],
+      },
+    ]
+  },
   poweredByHeader: false,
   output: "standalone",
 }
